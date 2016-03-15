5146090790e45d5b42eb6415a04fad5a4879b3ed
Phoenix787 <shiny777@bk.ru>
2016-03-15 12:29:46 +0500

    change gamecontrol and it's dependencies

---
+++ src/main/java/L8_socket_thread/ex2/Main.java
@@ -0,0 +1,14 @@
+package L8_socket_thread.ex2;
+
+/**
+ * Created by Сергеева on 15.03.2016.
+ * два класса: один будет ставить флаг, другой снимать
+ */
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
