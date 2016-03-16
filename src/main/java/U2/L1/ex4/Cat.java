9e8f829689efc3e73ce1590937fa9d593a08a168
Phoenix787 <shiny777@bk.ru>
2016-03-16 11:01:00 +0500

    generics level2

--- src/main/java/U2/L1/ex4/Cat.java
+++ src/main/java/U2/L1/ex4/Cat.java
@@ -3,5 +3,10 @@ package U2.L1.ex4;
 /**
  * Created by Сергеева on 16.03.2016.
  */
-public class Cat extends Animal {
+public class Cat extends Animal implements Miauable {
+
+
+
+    public Cat() {
+    }
 }
--- src/main/java/U2/L1/ex4/Gen3.java
+++ src/main/java/U2/L1/ex4/Gen3.java
@@ -7,6 +7,10 @@ package U2.L1.ex4;
 public class Gen3<T extends Animal & Miauable> {
     T object;
 
+    public Gen3(T object) {
+        this.object = object;
+    }
+
     void doubleValue(){
         double d = object.doubleValue();
         System.out.println(d);
