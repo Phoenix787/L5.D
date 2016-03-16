590822b18178fca9d5cd35504418b5731db69d23
Phoenix787 <shiny777@bk.ru>
2016-03-16 10:57:19 +0500

    change gamecontrol and it's dependencies

---
+++ src/main/java/U2/L1/ex4/Cat.java
@@ -0,0 +1,7 @@
+package U2.L1.ex4;
+
+/**
+ * Created by Сергеева on 16.03.2016.
+ */
+public class Cat extends Animal {
+}
---
+++ src/main/java/U2/L1/ex4/Gen3.java
@@ -0,0 +1,14 @@
+package U2.L1.ex4;
+
+/**
+ * Created by Сергеева on 16.03.2016.
+ *
+ */
+public class Gen3<T extends Animal & Miauable> {
+    T object;
+
+    void doubleValue(){
+        double d = object.doubleValue();
+        System.out.println(d);
+    }
+}
